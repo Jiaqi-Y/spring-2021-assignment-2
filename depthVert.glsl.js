75ca99debac257b2befafd87060d05cbf67b429a
Jiaqi-Y <77703776+Jiaqi-Y@users.noreply.github.com>
2021-04-11 21:01:50 -0500

    Update depthVert.glsl.js

--- depthVert.glsl.js
+++ depthVert.glsl.js
@@ -1,10 +1,13 @@
 export default `#version 300 es
 
 in vec3 position;
+in vec2 texcoord;
 out vec2 vTexcoord;
 
 void main() {
-    gl_Position = vec4(position, 1);
+    gl_Position = vec4(position, 1.0);
     // TODO: send vTexcoord to fragment shader with texture coordinates
+	vTexcoord = texcoord;
+	
 }    
 `;
